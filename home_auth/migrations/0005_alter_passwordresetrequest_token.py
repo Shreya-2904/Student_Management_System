4309212ab86653ea9546710fb479ec0aec5644d2
SHREYA SHUKLA <shuklashreya2904@gmail.com>
2025-07-30 16:56:20 +0530

    Create 0005_alter_passwordresetrequest_token.py

---
+++ home_auth/migrations/0005_alter_passwordresetrequest_token.py
@@ -0,0 +1,18 @@
+from django.db import migrations, models
+class Migration(migrations.Migration):
+    dependencies = [
+        ("home_auth", "0004_alter_passwordresetrequest_token"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="passwordresetrequest",
+            name="token",
+            field=models.CharField(
+                default="kNl6MPiOJ8wj1nd6odXT0DqBY7h43UWW",
+                editable=False,
+                max_length=32,
+                unique=True,
+            ),
+        ),
+    ]
